3fc5a3a2067c5b51ef67f7cd8f0836f6d7646b22
Regolin542 <74542599+Regolin542@users.noreply.github.com>
2020-11-16 23:15:37 +0100

    Create TiJ_This.java

---
+++ TiJ_This.java
@@ -0,0 +1,20 @@
+package TiJ;
+
+class Apricot{
+	public void pick() {
+		pit();
+	}
+	public void pit() {
+		System.out.println("123");
+	}
+}
+
+
+public class TiJ_This {
+
+	public static void main(String[] args) {
+		Apricot a = new Apricot();
+		a.pit();
+	}
+
+}
